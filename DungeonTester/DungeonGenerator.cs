67c924ac25331abbecaa2cf396964ab3a3a75869
Christopher "Claith" Smith <csmith49@fau.edu>
2015-09-19 23:05:35 -0400

    Skeleton Program
    Lots to do still

---
+++ DungeonTester/DungeonGenerator.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Collections
+
+namespace DungeonTester
+{
+    /// <summary>
+    /// Creates and manages data regarding the dungeon as a whole.
+    /// Passes size and noise properties to create a dungeon floor to FloorMap.
+    /// Handles assigning rooms and reading generated noise.
+    /// </summary>
+    class DungeonGenerator
+    {
+        Boolean initialized = false;
+
+        UInt32 width = 1024;
+        public UInt32 Width
+        {
+            get { return width; }
+            protected set { width = value; }
+        }
+        
+        UInt32 height = 1024;
+        public UInt32 Height
+        {
+            get { return height; }
+            protected set { height = value; }
+        }
+
+        /// <summary>
+        /// A metric for describing the number of rooms.
+        /// A higher value generates many small rooms, and a smaller value generates more larger rooms.
+        /// When applied to a FloorMap, it changes the noise value to the ceiling of the (noise_range / complexity) multiple.
+        /// </summary>
+        int complexity = 4;
+        public int Complexity
+        {
+            get { return complexity; }
+            protected set { complexity = value; }
+        }
+
+        /// <summary>
+        /// The value by which smaller noise values will be culled from being considered rooms (set to 0)
+        /// This value is not superceded by Complexity, and this value can cull into the Complexity algorithm if not low enough.
+        /// </summary>
+        Double roomNoiseThreshold;
+        public Double RoomNoiseThreshold
+        {
+            get { return this.roomNoiseThreshold; }
+            protected set { this.roomNoiseThreshold = value; }
+        }
+
+        /// <summary>
+        /// Base seed to use to generate the whole dungeon.
+        /// Used to generate floor seeds
+        /// </summary>
+        int seed;
+        public int Seed
+        {
+            get { return this.seed; }
+            protected set { this.seed = value; }
+        }
+
+        Random rand;
+        protected Random Rand
+        {
+            get { return this.rand; }
+            set { this.rand = value; }
+        }
+
+        //There is a lot more boundary checking I can and should do for those 4 below
+        //Check is min is bigger than max, and vice versa
+        //a negative will just throw a compile error, so shouldn't be a problem
+        //Attached default values are arbitrary
+        UInt32 minWidth = 4;
+        public UInt32 MinWidth
+        {
+            get { return minWidth; }
+            protected set { minWidth = ( value < 3 ) ? 3 : value; }
+        }
+
+        UInt32 minHeight = 4;
+        public UInt32 MinHeight
+        {
+            get { return minHeight; }
+            protected set { minHeight = ( value < 3 ) ? 3 : value; }
+        }
+
+        UInt32 maxWidth = 32;
+        public UInt32 MaxWidth
+        {
+            get { return maxWidth; }
+            protected set { maxWidth = (value > width ) ? width : value; }
+        }
+
+        UInt32 maxHeight = 32;
+        public UInt32 MaxHeight
+        {
+            get { return maxHeight; }
+            protected set { maxHeight = ( value > height ) ? height : value; }
+        }
+
+        List<FloorMap> floors;
+
+        public Boolean setupDungeon(UInt32 DungeonHeight = 1024, UInt32 DungeonWidth = 1024, int RoomComplexity = 4, 
+            UInt32 RoomMinWidth = 4, UInt32 RoomMinHeight = 4, int seed = 0)
+        {
+            this.Complexity = RoomComplexity;
+            this.MinWidth = RoomMinWidth;
+            this.MinHeight = RoomMinHeight;
+            this.Width = DungeonWidth;
+            this.Height = DungeonHeight;
+
+            this.Seed = seed;
+
+            this.floors = new List<FloorMap>(); //think about pre-defining capacity
+
+            //Need to properly initialize here
+
+            initialized = true;
+            return initialized;
+        }
+
+        public Boolean nextFloor(UInt32 FloorWidth = 1024, UInt32 FloorHeight = 1024, int Seed = -1)
+        {
+            //This current setup only supports one floor size. Allow setup to have nextWidth and such, and add floor dynamically
+            return true;
+        }
+
+        public void Generate()
+        {
+            //empty floor map
+            FloorMap tempFloor;// = new FloorMap(this.Width, this.Height);
+
+            UInt32 scaledWidth = this.Width / this.MinWidth;
+            UInt32 scaledHeight = this.Height / this.MinHeight;
+            FloorMap scaledFloor = new FloorMap(scaledWidth, scaledHeight, this.Complexity);
+            //get noise type from MainWindow
+
+            //Using Random for testing purposes
+            FloorMap.NoiseTypes noiseType = FloorMap.NoiseTypes.Random;
+
+            scaledFloor.GenerateFloor(noiseType, 0.5);
+
+            //Fill out the tempFloor by multiplying the coords by the scaling factor
+            tempFloor = ExpandFloor(scaledFloor);
+
+            floors.Add(tempFloor);
+        }
+
+        protected FloorMap ExpandFloor(FloorMap sourceFloor)
+        {
+            //Tempted to make a constructor to do this for me automatically. Might not be a bad idea.
+            //Think about making use of global scaling instead of per axis
+            FloorMap tempFloor = new FloorMap(sourceFloor.Width * this.MinWidth, sourceFloor.Height * this.MinHeight, sourceFloor.Complexity, sourceFloor.Seed);
+
+            //how do I want to do this? It would likely be best to simply loop through tempFloor, and read the value from (ushort)floor(ix / scale) and (ushort)floor(iy / scale)
+
+            for (UInt32 ix = 0; ix < tempFloor.Width; ix++)
+            {
+                for (UInt32 iy = 0; iy < tempFloor.Height; iy++)
+                {
+                    //Read the value -- Don't like this multiple casting. Look into a redesign.
+                    //Might be better to read the sourceFloor once, then apply the value using another set of for loops.
+                    //If the scaling factor was set, could hard code it in. Think about doing that for small standard scaling (4 or 8).
+                    tempFloor.SetPoint(ix, iy, sourceFloor.Grid[(UInt32)Math.Floor((double)ix / this.MinWidth), (UInt32)Math.Floor((double)iy / this.MinHeight)]);
+
+                    //This will work for a rough start
+                }
+            }
+
+            return tempFloor;
+        }
+    }
+}
---
+++ DungeonTester/FloorMap.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonTester
+{
+    public class FloorMap
+    {
+        //This about defining a value type, such as UInt16 for the FloorMap, so changing data sizes is easier down the road
+        //Can't see any reason to go beyond UInt16 for range of indexes, reduces memory footprint in half if done
+
+        public const UInt32 MIN_FLOOR_WIDTH      = 4;
+        public const UInt32 MIN_FLOOR_HEIGHT     = 4;
+        public const UInt32 DEFAULT_FLOOR_WIDTH  = 1024;
+        public const UInt32 DEFAULT_FLOOR_HEIGHT = 1024;
+        public const UInt32 MAX_FLOOR_WIDTH      = 4096;
+        public const UInt32 MAX_FLOOR_HEIGHT     = 4096;
+        public const int DEFAULT_COMPLEXITY      = 4;
+
+        public enum NoiseTypes { Perlin, Simplex, Gradient, Random };
+
+        Boolean initialized = false;
+        public Boolean Initialized
+        {
+            get { return this.initialized; }
+            protected set { this.initialized = value; }
+        }
+
+        ushort[,] grid;
+        public ushort[,] Grid
+        {
+            get { return this.grid; }
+            protected set { this.grid = value; }
+        }
+
+        UInt32 width;
+        public UInt32 Width
+        {
+            get { return this.width; }
+            protected set { this.width = value; }
+        }
+
+        UInt32 height;
+        public UInt32 Height
+        {
+            get { return this.height; }
+            protected set { this.height = value; }
+        }
+
+        int complexity;
+        public int Complexity
+        {
+            get { return this.complexity; }
+            protected set { this.complexity = value; }
+        }
+
+        protected Random rand;
+
+        int seed;
+        public int Seed
+        {
+            get { return this.seed; }
+            protected set { this.seed = value; }
+        }
+
+        public FloorMap(UInt32 floorWidth = FloorMap.DEFAULT_FLOOR_WIDTH, UInt32 floorHeight = FloorMap.DEFAULT_FLOOR_HEIGHT, int complexity = FloorMap.DEFAULT_COMPLEXITY, int seed = 0)
+        {
+            if (floorWidth < FloorMap.MIN_FLOOR_WIDTH || floorWidth > FloorMap.MAX_FLOOR_WIDTH)
+            {
+                Console.Out.WriteLine("FloorMap::FloorMap(UInt32 floorWidth, UInt32 floorHeight) -- Invalid floorWidth -- Set to Default");
+                floorWidth = FloorMap.DEFAULT_FLOOR_WIDTH;
+            }
+
+            if (floorHeight < FloorMap.MIN_FLOOR_HEIGHT || floorHeight > FloorMap.MAX_FLOOR_HEIGHT)
+            {
+                Console.Out.WriteLine("FloorMap::FloorMap(UInt32 floorWidth, UInt32 floorHeight) -- Invalid floorHeight -- Set to Default");
+                floorHeight = FloorMap.DEFAULT_FLOOR_HEIGHT;
+            }
+
+            this.Width = floorWidth;
+            this.Height = floorHeight;
+            
+            this.Grid = new ushort[this.Width, this.Height];
+
+            this.Complexity = complexity;
+            this.Seed = seed;
+            this.rand = new Random(this.Seed);
+        }
+
+        /// <summary>
+        /// Deep Copy
+        /// </summary>
+        /// <param name="copyFrom">Object to Copy From</param>
+        public FloorMap(FloorMap copyFrom)
+        {
+            this.Grid = copyFrom.Grid;
+        }
+
+        public Boolean Clear(ushort newValue = default(ushort))
+        {
+            //No need to check for threading issues
+            if (newValue == default(ushort))
+            {
+                this.Grid.Initialize();
+                return true;
+            }
+
+            //Might want to check for initialization instead of bounds, as one is required for the other and would reduce a step
+            //Check Bounds
+            if (this.Width > 0 && this.Height > 0)
+            {
+                //foreach for reading
+                //for for writing
+                for (UInt32 ix = 0; ix < this.Width; ix++)
+                {
+                    for (UInt32 iy = 0; iy < this.Height; iy++)
+                    {
+                        this.Grid[ix, iy] = newValue;
+                    }                                    
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Generates a FloorMap object, and initializes it
+        /// </summary>
+        /// <param name="noise">The type of Noise algorithm to use</param>
+        /// <param name="noiseThreshold">The value that the noise has to be above to generate a room</param>
+        /// <param name="complexity">The value that controls the parsing of rooms from the noise</param>
+        /// <returns>True if the floor is generated completely</returns>
+        public Boolean GenerateFloor(NoiseTypes noise, Double noiseThreshold)
+        {
+            // Add some default values to the parameters
+
+            //can't call if the object is null, so don't need to check
+
+            //Sizes were taken care of previously during creation of object, assume they are safe
+
+            //grid value = floor(noise_value * (Complexity + 1))
+
+            switch (noise)
+            {
+                case NoiseTypes.Perlin:
+                    this.Initialized = this.generatePerlinNoise(noiseThreshold, complexity);
+                    break;
+                case NoiseTypes.Simplex:
+                    this.Initialized = this.generateSimplexNoise(noiseThreshold, complexity);
+                    break;
+                case NoiseTypes.Gradient:
+                    this.Initialized = this.generateGradientNoise(noiseThreshold, complexity);
+                    break;
+                case NoiseTypes.Random:
+                    this.Initialized = this.generateRandomNoise(noiseThreshold);
+                    break;
+                default:
+                    //Response with an error message
+                    this.Initialized = false;
+                    break;
+            }
+
+            return this.Initialized;
+        }
+
+        protected Boolean generatePerlinNoise(Double noiseThreshold, int complexity)
+        {
+            return false;
+        }
+
+        protected Boolean generateSimplexNoise(Double noiseThreshold, int complexity)
+        {
+            return false;
+        }
+
+        protected Boolean generateGradientNoise(Double noiseThreshold, int complexity)
+        {
+            return false;
+        }
+
+        protected Boolean generateRandomNoise(Double noiseThreshold)
+        {
+            //Easiest to implement and see any flaws with the system, so it is made first
+
+            int comp = this.Complexity + 1; //Don't need to use this value any where else in this scope
+
+            Random rand = new Random(this.Seed);
+
+            for (UInt32 ix = 0; ix < this.Width; ix++)
+            {
+                for (UInt32 iy = 0; iy < this.Height; iy++)
+                {
+                    this.Grid[ix, iy] = (ushort)rand.Next(comp);
+                }
+            }
+
+            return true; //Hard to fail this method
+
+            //Could try / catch, but that only accomplish a slower system and no additional functionality
+        }
+
+        /// <summary>
+        /// Replaces a previous value with a new value
+        /// </summary>
+        /// <param name="floorX">First Dimension</param>
+        /// <param name="floorY">Second Dimension</param>
+        /// <param name="newValue">Value to apply</param>
+        /// <returns></returns>
+        public Boolean SetPoint(UInt32 floorX, UInt32 floorY, ushort newValue)
+        {
+            //Check Bounds -- Doesn't work when dealing with the scaled floor grid -- check later if || or && is faster
+            if (floorX < 0 || floorX > this.Width)
+            {
+                Console.Out.WriteLine("FloorMap::SetPoint(UInt32 floorX, UInt32 floorY, ushort newValue) -- Invalid floorX -- Out of Bounds");
+                return false;
+            }
+
+            if (floorY < 0 || floorY > this.Height)
+            {
+                Console.Out.WriteLine("FloorMap::SetPoint(UInt32 floorX, UInt32 floorY, ushort newValue) -- Invalid floorY -- Out of Bounds");
+                return false;
+            }
+
+            this.Grid[floorX, floorY] = newValue;
+
+            return true;
+        }
+    }
+}
